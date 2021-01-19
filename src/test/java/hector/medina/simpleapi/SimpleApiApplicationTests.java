6dc90c8eebdc29aa9629ff7334ec792c3f6ae39c
Hector Mario Medina Cabanelas <hector.medina.cabanelas@gmail.com>
2021-01-19 11:59:02 +0100

    SpringBoot base added.

---
+++ src/test/java/hector/medina/simpleapi/SimpleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hector.medina.simpleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
