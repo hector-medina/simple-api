6088c584309659383facff001ec37c5d90ec1648
Hector Mario Medina Cabanelas <hector.medina.cabanelas@gmail.com>
2021-01-21 23:42:24 +0100

    FakePersonDataAccessPerson Added.

---
+++ src/main/java/hector/medina/simpleapi/dao/FakePersonDataAccessService.java
@@ -0,0 +1,17 @@
+package hector.medina.simpleapi.dao;
+
+import hector.medina.simpleapi.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class FakePersonDataAccessService implements PersonDao {
+    private static List<Person> DB = new ArrayList<>();
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        DB.add(new Person(id, person.getName()));
+        return 1;
+    }
+}
