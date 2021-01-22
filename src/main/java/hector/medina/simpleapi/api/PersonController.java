4884ef63dbf61b5e3504153a85fea30dc8617e94
Hector Mario Medina Cabanelas <hector.medina.cabanelas@gmail.com>
2021-01-22 11:48:08 +0100

    RequesMapping added to match the url

--- src/main/java/hector/medina/simpleapi/api/PersonController.java
+++ src/main/java/hector/medina/simpleapi/api/PersonController.java
@@ -4,8 +4,11 @@ import hector.medina.simpleapi.model.Person;
 import hector.medina.simpleapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("api/v2/person")
 @RestController
 public class PersonController {
     private final PersonService personService;
@@ -16,7 +19,7 @@ public class PersonController {
     }
 
     @PostMapping
-    public void addPerson(Person person){
+    public void addPerson(@RequestBody Person person){
         personService.addPerson(person);
     }
 }
