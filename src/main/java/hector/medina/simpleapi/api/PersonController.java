44f7031581c9c002eda969c19f1f49a019c0ae91
Hector Mario Medina Cabanelas <hector.medina.cabanelas@gmail.com>
2021-01-22 11:41:02 +0100

    Json added on Controller

--- src/main/java/hector/medina/simpleapi/api/PersonController.java
+++ src/main/java/hector/medina/simpleapi/api/PersonController.java
@@ -3,7 +3,10 @@ package hector.medina.simpleapi.api;
 import hector.medina.simpleapi.model.Person;
 import hector.medina.simpleapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PersonController {
     private final PersonService personService;
 
@@ -12,6 +15,7 @@ public class PersonController {
         this.personService = personService;
     }
 
+    @PostMapping
     public void addPerson(Person person){
         personService.addPerson(person);
     }
