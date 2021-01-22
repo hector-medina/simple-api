82e426fb0b3a359023855c722cde9f2241fd8db7
Hector Mario Medina Cabanelas <hector.medina.cabanelas@gmail.com>
2021-01-22 12:28:44 +0100

    GetAllPerson API endpoint implemented.

--- src/main/java/hector/medina/simpleapi/api/PersonController.java
+++ src/main/java/hector/medina/simpleapi/api/PersonController.java
@@ -3,12 +3,11 @@ package hector.medina.simpleapi.api;
 import hector.medina.simpleapi.model.Person;
 import hector.medina.simpleapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("api/v2/person")
+import java.util.List;
+
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
     private final PersonService personService;
@@ -21,5 +20,10 @@ public class PersonController {
     @PostMapping
     public void addPerson(@RequestBody Person person){
         personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
     }
 }
--- src/main/java/hector/medina/simpleapi/dao/PersonDao.java
+++ src/main/java/hector/medina/simpleapi/dao/PersonDao.java
@@ -2,6 +2,7 @@ package hector.medina.simpleapi.dao;
 
 import hector.medina.simpleapi.model.Person;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -12,4 +13,6 @@ public interface PersonDao {
         UUID id = UUID.randomUUID();
         return insertPerson(id, person);
     }
+
+    List<Person> selectAllPeople();
 }
